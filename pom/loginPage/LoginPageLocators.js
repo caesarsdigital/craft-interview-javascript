30580e5d2bff365eeb1b92c3a298bb6d40ef77f1
Jayson Lacey <jlacey@williamhill.us>
2024-01-17 19:20:04 +0000

    Update craft

--- pom/loginPage/LoginPageLocators.js
+++ pom/loginPage/LoginPageLocators.js
@@ -1,4 +1,4 @@
 // Login Page
 export const APP_HEADER = '[data-qa=app-header]';
-export const ERROR_MESSAGE = '.mwc-form-error-message-text';
+export const ERROR_MESSAGE = '.login-content__error-details';
 // What locator strategies are you familiar with? Any pros/cons?
